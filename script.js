dec6e9b1338767357846cf52dcfd222238965e45
Alex Sandro <108097026+alexsandro49@users.noreply.github.com>
2022-12-21 16:52:04 -0300

    Exercícios dia 21/12

---
+++ script.js
@@ -0,0 +1,8 @@
+const asideBtn = document.getElementsByClassName('btn')[0];
+const asideUl = document.getElementsByTagName('ul')[0];
+
+
+asideBtn.addEventListener('click', () => {
+    asideBtn.classList.toggle('actived');
+    asideUl.classList.toggle('actived');
+});
